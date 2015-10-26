c8e22d8e704c2aacb6b451bfc9ab12004e25e8ea
usr_share <usrshare@hashbang.sh>
2015-10-26 07:30:37 +0000

    linking with openssl?

---
+++ src/pwdhash.c
@@ -0,0 +1,4 @@
+// vim: cin:sts=4:sw=4 
+#include "pwdhash.h"
+
+#include <openssl/evp.h>
---
+++ src/pwdhash.h
@@ -0,0 +1,7 @@
+// vim: cin:sts=4:sw=4 
+#ifndef PWDHASH_H
+#define PWDHASH_H
+
+int hash_pwd(int alg, const char* restrict pwd, char* o_hash);
+
+#endif
